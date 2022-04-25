51bcbe4d730c235a4eb3201fb8dff85d88d64246
AndyLow14 <andyjuncehgnlow@gmail.com>
2022-04-25 21:22:37 +1000

    Added AI Pathfinding

--- Snake.py
+++ Snake.py
@@ -1,16 +1,19 @@
 import sys
+from turtle import distance
 import pygame
 import time
 import random
+from math import dist
+from operator import add
 
 # Snake move speed
-snake_speed = 15
+snake_speed = 500
 
 # Window Size
 window_x = 720
 window_y = 480
 
-dark_gray = pygame.Color(0, 0, 0)
+dark_gray = pygame.Color(18, 18, 18)
 white = pygame.Color(255, 255, 255)
 orange = pygame.Color(255, 150, 113)
 
@@ -34,43 +37,61 @@ food_spawn = True
 direction = 'RIGHT'
 change_to = direction
 
+# Available Moves
+moves = [[0,-10], #UP
+        [0,10],  #DOWN
+        [-10,0], #LEFT
+        [10,0]]  #RIGHT
+
 # Main function
 while True:
 
-    # Detect actions
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    # # Detect actions
+    # for event in pygame.event.get():
+    #     if event.type == pygame.QUIT:
+    #         pygame.quit()
+    #         sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                change_to = 'UP'
-            if event.key == pygame.K_DOWN:
-                change_to = 'DOWN'
-            if event.key == pygame.K_LEFT:
-                change_to = 'LEFT'
-            if event.key == pygame.K_RIGHT:
-                change_to = 'RIGHT'
+    #     if event.type == pygame.KEYDOWN:
+    #         if event.key == pygame.K_UP:
+    #             change_to = 'UP'
+    #         if event.key == pygame.K_DOWN:
+    #             change_to = 'DOWN'
+    #         if event.key == pygame.K_LEFT:
+    #             change_to = 'LEFT'
+    #         if event.key == pygame.K_RIGHT:
+    #             change_to = 'RIGHT'
 
-    if change_to == 'UP' and direction != 'DOWN':
-        direction = 'UP'
-    if change_to == 'DOWN' and direction != 'UP':
-        direction = 'DOWN'
-    if change_to == 'LEFT' and direction != 'RIGHT':
-        direction = 'LEFT'
-    if change_to == 'RIGHT' and direction != 'LEFT':
-        direction = 'RIGHT'
+    # if change_to == 'UP' and direction != 'DOWN':
+    #     direction = 'UP'
+    # if change_to == 'DOWN' and direction != 'UP':
+    #     direction = 'DOWN'
+    # if change_to == 'LEFT' and direction != 'RIGHT':
+    #     direction = 'LEFT'
+    # if change_to == 'RIGHT' and direction != 'LEFT':
+    #     direction = 'RIGHT'
 
-    # Snake movement
-    if direction == 'UP':
-        snake_position[1] -= 10
-    if direction == 'DOWN':
-        snake_position[1] += 10
-    if direction == 'LEFT':
-        snake_position[0] -= 10
-    if direction == 'RIGHT':
-        snake_position[0] += 10
+    #  # Snake movement
+    # if shortest_path == 'UP':
+    #     snake_position[1] -= 10
+    # if shortest_path == 'DOWN':
+    #     snake_position[1] += 10
+    # if shortest_path == 'LEFT':
+    #     snake_position[0] -= 10
+    # if shortest_path == 'RIGHT':
+    #     snake_position[0] += 10
+
+    # AI Euclidean Pathfinding
+    shortest_path = moves[0]
+    shortest_dir = dist(list(map(add, snake_position, moves[0])), food_position)
+
+    for i in range(1,len(moves)):
+        distance = dist(list(map(add, snake_position, moves[i])), food_position)
+        if distance < shortest_dir:
+            shortest_path = moves[i]
+            shortest_dir = distance
+
+    snake_position = list(map(add, snake_position, shortest_path))
 
     snake_body.insert(0, list(snake_position))
     if snake_position[0] == food_position[0] and snake_position[1] == food_position[1]:
@@ -95,3 +116,4 @@ while True:
 
     pygame.display.update()
     fps.tick(snake_speed)
+
